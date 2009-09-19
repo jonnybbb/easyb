49c91ade5dff4f52a4999dc131fba05d459511f3
darran.m.white <darran.m.white@9bebd20b-ac44-0410-9e34-335f4663fabf>
2009-09-19 17:29:30 +0000

    Removed old folder which has now been renamed
    git-svn-id: http://easyb.googlecode.com/svn/trunk@772 9bebd20b-ac44-0410-9e34-335f4663fabf

--- eclipse-plugins/org.easyb.eclipse.ui/src/org/easyb/easybplugin/editor/BehaviourActionContributor.java
+++
@@ -1,7 +0,0 @@
-package org.easyb.easybplugin.editor;
-
-import org.eclipse.ui.editors.text.TextEditorActionContributor;
-
-public class BehaviourActionContributor extends TextEditorActionContributor {
-
-}
