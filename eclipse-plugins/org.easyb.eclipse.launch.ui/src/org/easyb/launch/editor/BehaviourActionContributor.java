d785fa9d5bc5584b5c2ab90740d853dd4657a73c
darran.m.white <darran.m.white@9bebd20b-ac44-0410-9e34-335f4663fabf>
2009-09-19 18:45:43 +0000

    Removed files from launch ui project which have been moved to the ui project Tidied up the manifests/bundle names
    git-svn-id: http://easyb.googlecode.com/svn/trunk@777 9bebd20b-ac44-0410-9e34-335f4663fabf

--- eclipse-plugins/org.easyb.eclipse.launch.ui/src/org/easyb/launch/editor/BehaviourActionContributor.java
+++
@@ -1,7 +0,0 @@
-package org.easyb.launch.editor;
-
-import org.eclipse.ui.editors.text.TextEditorActionContributor;
-
-public class BehaviourActionContributor extends TextEditorActionContributor {
-
-}
