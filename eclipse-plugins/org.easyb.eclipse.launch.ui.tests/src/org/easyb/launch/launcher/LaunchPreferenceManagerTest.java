80dff29d65076ba55db9729fefe5c82aa8776360
darran.m.white <darran.m.white@9bebd20b-ac44-0410-9e34-335f4663fabf>
2009-10-02 13:57:56 +0000

    Added launch preference manager tests
    git-svn-id: http://easyb.googlecode.com/svn/trunk@842 9bebd20b-ac44-0410-9e34-335f4663fabf

---
+++ eclipse-plugins/org.easyb.eclipse.launch.ui.tests/src/org/easyb/launch/launcher/LaunchPreferenceManagerTest.java
@@ -0,0 +1,39 @@
+package org.easyb.launch.launcher;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.easyb.launch.preference.LaunchPreferenceManager;
+
+import junit.framework.TestCase;
+
+public class LaunchPreferenceManagerTest extends TestCase {
+	private static final String EASYB_JAR = "test_easyb.jar";
+	private static final String COMMONS_JAR = "test_commons.jar";
+	private static final String GROOVY_JAR = "test_groovy.jar";
+	private static final String FILE_REGEX = "commons-cli-[\\d|.]*jar,\\s*easyb-[\\d|.]*jar,\\s*groovy-[\\d|.]*jar";
+	private static final Pattern regexPattern = Pattern.compile(FILE_REGEX);
+		
+	public void testLaunchPreferences()throws Exception{
+		LaunchPreferenceManager.setCommoncCLIJarLocation(COMMONS_JAR);
+		LaunchPreferenceManager.setEasybJarLocation(EASYB_JAR);
+		LaunchPreferenceManager.setGroovyJarLocation(GROOVY_JAR);
+		
+		this.assertEquals(COMMONS_JAR,LaunchPreferenceManager.getCommoncCLIJar());
+		this.assertEquals(EASYB_JAR,LaunchPreferenceManager.getEasybJarLocation());
+		this.assertEquals(GROOVY_JAR,LaunchPreferenceManager.getGroovyJarLocation());
+		
+		String jars = LaunchPreferenceManager.getFormattedDefaultJarNames();
+		
+		Matcher matcher = regexPattern.matcher(jars);
+		
+		this.assertTrue(matcher.find());
+		
+		LaunchPreferenceManager.setDefaults();
+		
+		this.assertEquals("",LaunchPreferenceManager.getCommoncCLIJar());
+		this.assertEquals("",LaunchPreferenceManager.getEasybJarLocation());
+		this.assertEquals("",LaunchPreferenceManager.getGroovyJarLocation());
+
+	}
+}
