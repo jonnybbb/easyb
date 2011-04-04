a7b1047eaaebd4e6545b41cbcce53aa216c32de7
Johannes Bühler <johannes.buehler@canoo.com>
2011-04-04 23:02:55 +0200

    added project test classes

--- gradle-easyb-test/src/main/groovy/foo/Foo.java
+++
@@ -1,7 +0,0 @@
-package foo;
-
-/**
- * Created by IntelliJ IDEA. User: johannes Date: 31/03/11 Time: 23:32 To change this template use File | Settings | File Templates.
- */
-public class Foo {
-}
---
+++ gradle-easyb-test/src/main/groovy/foo2/Foo2.java
@@ -0,0 +1,12 @@
+package foo2;
+
+/**
+ * Created by IntelliJ IDEA. User: johannes Date: 01/04/11 Time: 00:30 To change this template use File | Settings | File Templates.
+ */
+public class Foo2 {
+
+
+    public Foo2() {
+        System.out.println("Hurry i was loaded");
+    }
+}
